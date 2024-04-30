a0dc1d0e88ba8e35c223edf79333c858772891a8
NagendranHarini <168558743+NagendranHarini@users.noreply.github.com>
2024-04-30 23:23:46 +0530

    Update file_1.c

--- file_1.c
+++ file_1.c
@@ -4,9 +4,9 @@ int main () {
 	printf("Enter two integers: ");
 	scanf("%d %d", &number1, &number2);
 //calculate the sum
-        sum=number1 + number2;
-        printf("%d + %d = %d",number1, number2, sum);
-        return 0;
+sum=number1 + number2;
+printf("%d + %d = %d",number1, number2, sum);
+return 0;
 }
 
 
